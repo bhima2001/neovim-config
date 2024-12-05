0d41d3278226a4bacae70ca5fbc86e1d6ebb5f57
bhima2001 <shankardb369@gmail.com>
2024-12-05 09:46:31 +0000

    made some modification in the variables and group creation, also added remaps and set

--- lua/bhima2001/lazy/fugitive.lua
+++ lua/bhima2001/lazy/fugitive.lua
@@ -3,11 +3,11 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-        local bhima_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+        local bhima_Fugitive = vim.api.nvim_create_augroup("bhima2001fugitive", {})
 
         local autocmd = vim.api.nvim_create_autocmd
         autocmd("BufWinEnter", {
-            group = bhima_Fugitive, 
+            group = bhima_Fugitive,
             pattern = "*",
             callback = function()
                 if vim.bo.ft ~= "fugitive" then
