0cfb618f075fcc5599c517ba7f7494b5415c126b
bhima2001 <shankardb369@gmail.com>
2025-01-31 11:45:28 +0000

    removed some remaps in remap.lua and also added neotree for better file system

--- lua/bhima2001/lazy/init.lua
+++ lua/bhima2001/lazy/init.lua
@@ -1,14 +1,14 @@
 return {
-	"nvim-lua/plenary.nvim",
-	{
-		"folke/trouble.nvim",
-		config = function ()
-			require("trouble").setup({
-				icons = false,
-			})
-		end
-	},
-	"eandrju/cellular-automaton.nvim",
-	'github/copilot.vim',
-	'ThePrimeagen/vim-apm'
+    "nvim-lua/plenary.nvim",
+    {
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup({
+                icons = false,
+            })
+        end
+    },
+    "eandrju/cellular-automaton.nvim",
+    'github/copilot.vim',
+    'ThePrimeagen/vim-apm'
 }
